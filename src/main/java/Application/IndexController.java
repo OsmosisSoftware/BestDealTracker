a536c00d8557241775f90cf3f57cb5710f3944b6
Yago <y_rodrigez@hotmail.com>
2016-05-22 11:37:21 +0200

    Añadidas dependencias al Maven Polymer, jquery jacoco,

---
+++ src/main/java/Application/IndexController.java
@@ -0,0 +1,13 @@
+package Application;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class IndexController {
+  @RequestMapping("/")
+  public String index(){
+    return "index";
+  }
+}
