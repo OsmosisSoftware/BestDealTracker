e6104932865ad3ad0b58b65c261be05a1a6e1486
yago <y_rodrigez@hotmail.com>
2016-06-07 12:37:44 +0200

    Creada clase usuario
    Solo username y password.

--- src/main/java/org/osmosis/entities/User.java
+++ src/main/java/org/osmosis/entities/User.java
@@ -2,7 +2,10 @@ package org.osmosis.entities;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.List;
 
 @Document
 public class User {
@@ -11,9 +14,30 @@ public class User {
     private String user;
     private String password;
 
+    @DBRef
+    private List<AmazonItem> myAmazonItems;
+
+    @DBRef
+    private List<PcComponentesItem> myPcComponentesItems;
+
     public User(){}
 
-    public User(String user, String password){
+    public User(
+        String user,
+        String password,
+        List<AmazonItem> myAmazonItems,
+        List<PcComponentesItem> myPcComponentesItems
+    ) {
+        this.setUser(user);
+        this.setPassword(password);
+        this.setMyAmazonItems(myAmazonItems);
+        this.setMyPcComponentesItems(myPcComponentesItems);
+    }
+
+    public User(
+        String user,
+        String password
+    ) {
         this.setUser(user);
         this.setPassword(password);
     }
@@ -37,5 +61,37 @@ public class User {
 
     public String getUser() {
         return user;
+    }
+
+    public void setMyAmazonItems(List<AmazonItem> myAmazonItems){
+        if (myAmazonItems == null) throw new NullPointerException();
+
+        this.myAmazonItems = myAmazonItems;
+    }
+
+    public void setMyPcComponentesItems(List<PcComponentesItem> myPcComponentesItems){
+        if (myPcComponentesItems == null) throw new NullPointerException();
+
+        this.myPcComponentesItems = myPcComponentesItems;
+    }
+
+    public List<AmazonItem> getMyAmazonItems(){
+        return this.myAmazonItems;
+    }
+
+    public List<PcComponentesItem> getMyPcComponentesItems(){
+        return this.myPcComponentesItems;
+    }
+
+    public void addAmazonItem(AmazonItem amazonItem){
+        if (amazonItem == null || amazonItem.getAmazonItemId().equals("")) throw new IllegalArgumentException("");
+
+        this.myAmazonItems.add(amazonItem);
+    }
+
+    public void addPcComponentesItem(PcComponentesItem pcComponentesItem){
+        if (pcComponentesItem == null || pcComponentesItem.getPcComponentesItemId().equals("")) throw new IllegalArgumentException("");
+
+        this.myPcComponentesItems.add(pcComponentesItem);
     }
 }
